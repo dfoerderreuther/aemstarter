602cbab73f65d8c695d6a6556c5243f0d5fb0ae8
Dominik Förderreuther <dominik@eleon.de>
2025-05-24 22:58:00 +0200

    MainActions

--- src/renderer/components/MainActionsView.tsx
+++ src/renderer/components/MainActionsView.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Group, Button, Modal, Stack, Text, Divider } from '@mantine/core';
-import { IconPlayerPlay, IconPlayerStop, IconDownload, IconSkull } from '@tabler/icons-react';
+import { Group, Button, Modal, Stack, Text, Paper, Tooltip, Badge, Divider } from '@mantine/core';
+import { IconPlayerPlay, IconPlayerStop, IconDownload, IconSkull, IconPackage } from '@tabler/icons-react';
 import { InstallService } from '../services/installService';
 import { Project } from '../../types/Project';
 
@@ -30,96 +30,204 @@ export const MainActionsView: React.FC<MainActionsViewProps> = ({ project }) => 
     }
   };
 
+  const sectionStyles = {
+    padding: '12px',
+    background: 'none',
+  };
+
+  const buttonStyles = {
+    root: { 
+      height: 30,
+      width: 30,
+      padding: 0,
+    }
+  };
+
+  const installButtonStyles = {
+    root: { 
+      height: 30,
+      padding: '0 12px',
+    }
+  };
+
   return (
     <>
-      <Group p="xs" gap="xs">
-        <Button 
-          color="blue" 
-          variant="filled" 
-          size="xs"
-          leftSection={<IconPlayerPlay size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Start all
-        </Button>
+      <Group align="flex-start" gap="md">
         
-        <Button 
-          color="dark" 
-          variant="outline" 
-          size="xs"
-          leftSection={<IconPlayerStop size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Stop all
-        </Button>
-        
-        <Button 
-          color="orange" 
-          variant="filled" 
-          size="xs"
-          leftSection={<IconSkull size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Kill all
-        </Button>
+        <Paper style={sectionStyles}>
+          <Stack gap="xs">
+            <Text size="sm" fw={500} c="dimmed">All</Text>
+            <Button.Group>
+              <Tooltip label="Start all">
+                <Button 
+                  color="blue" 
+                  variant="filled" 
+                  size="xs"
+                  styles={buttonStyles}
+                >
+                  <IconPlayerPlay size={16} />
+                </Button>
+              </Tooltip>
+              
+              <Tooltip label="Stop all">
+                <Button 
+                  color="dark" 
+                  variant="outline" 
+                  size="xs"
+                  styles={buttonStyles}
+                >
+                  <IconPlayerStop size={16} />
+                </Button>
+              </Tooltip>
+              
+              <Tooltip label="Kill all">
+                <Button 
+                  color="orange" 
+                  variant="filled" 
+                  size="xs"
+                  styles={buttonStyles}
+                >
+                  <IconSkull size={16} />
+                </Button>
+              </Tooltip>
+            </Button.Group>
+          </Stack>
+        </Paper>
 
         <Divider orientation="vertical" />
 
-        <Button 
-          color="blue" 
-          variant="filled" 
-          size="xs"
-          leftSection={<IconPlayerPlay size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Start author
-        </Button>
+        <Paper style={sectionStyles}>
+          <Stack gap="xs">
+            <Group justify="space-between" align="center">
+              <Text size="sm" fw={500} c="dimmed">Author</Text>
+              <Badge variant="light" color="red" size="sm">232</Badge>
+            </Group>
+            <Group gap="xs">
+              <Button.Group>
+                <Tooltip label="Start author">
+                  <Button 
+                    color="blue" 
+                    variant="filled" 
+                    size="xs"
+                    styles={buttonStyles}
+                  >
+                    <IconPlayerPlay size={16} />
+                  </Button>
+                </Tooltip>
 
-        <Button
-          color="red" 
-          variant="filled" 
-          size="xs"
-          leftSection={<IconPlayerStop size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Stop author
-        </Button>
+                <Tooltip label="Stop author">
+                  <Button
+                    color="red" 
+                    variant="filled" 
+                    size="xs"
+                    styles={buttonStyles}
+                  >
+                    <IconPlayerStop size={16} />
+                  </Button>
+                </Tooltip>
+              </Button.Group>
+
+            </Group>
+          </Stack>
+        </Paper>
+
+        <Divider orientation="vertical" />
+
+        <Paper style={sectionStyles}>
+          <Stack gap="xs">
+            <Group justify="space-between" align="center">
+              <Text size="sm" fw={500} c="dimmed">Publisher</Text>
+              <Badge variant="light" color="red" size="sm">232</Badge>
+            </Group>
+            <Group gap="xs">
+              <Button.Group>
+                <Tooltip label="Start publish">
+                  <Button 
+                    color="blue" 
+                    variant="filled" 
+                    size="xs"
+                    styles={buttonStyles}
+                  >
+                    <IconPlayerPlay size={16} />
+                  </Button>
+                </Tooltip>
+
+                <Tooltip label="Stop publish">
+                  <Button
+                    color="red" 
+                    variant="filled" 
+                    size="xs"
+                    styles={buttonStyles}
+                  >
+                    <IconPlayerStop size={16} />
+                  </Button>
+                </Tooltip>
+              </Button.Group>
+
+            </Group>
+          </Stack>
+        </Paper>
 
         <Divider orientation="vertical" />
 
-        <Button 
-          color="blue" 
-          variant="filled" 
-          size="xs"
-          leftSection={<IconPlayerPlay size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Start publish
-        </Button>
 
-        <Button
-          color="red" 
-          variant="filled" 
-          size="xs"
-          leftSection={<IconPlayerStop size={16} />}
-          styles={{ root: { height: 30 } }}
-        >
-          Stop publish
-        </Button>
+        <Paper style={sectionStyles}>
+          <Stack gap="xs">
+            <Group justify="space-between" align="center">
+              <Text size="sm" fw={500} c="dimmed">Dispatcher</Text>
+              <Badge variant="light" color="red" size="sm">232</Badge>
+            </Group>
+            <Group gap="xs">
+              <Button.Group>
+                <Tooltip label="Start publish">
+                  <Button 
+                    color="blue" 
+                    variant="filled" 
+                    size="xs"
+                    styles={buttonStyles}
+                  >
+                    <IconPlayerPlay size={16} />
+                  </Button>
+                </Tooltip>
+
+                <Tooltip label="Stop publish">
+                  <Button
+                    color="red" 
+                    variant="filled" 
+                    size="xs"
+                    styles={buttonStyles}
+                  >
+                    <IconPlayerStop size={16} />
+                  </Button>
+                </Tooltip>
+              </Button.Group>
+
+            </Group>
+          </Stack>
+        </Paper>
 
         <Divider orientation="vertical" />
-        
-        <Button 
-          color="blue" 
-          variant="outline" 
-          size="xs"
-          leftSection={<IconDownload size={16} />}
-          styles={{ root: { height: 30 } }}
-          onClick={handleInstall}
-          loading={isInstalling}
-        >
-          Install
-        </Button>
+
+        <Paper style={sectionStyles}>
+          <Stack gap="xs">
+            <Text size="sm" fw={500} c="dimmed">Other</Text>
+            <Button.Group>
+              <Tooltip label="Install">
+                <Button 
+                  color="blue" 
+                  variant="outline" 
+                  size="xs"
+                  styles={installButtonStyles}
+                  onClick={handleInstall}
+                  loading={isInstalling}
+                  leftSection={<IconDownload size={16} />}
+                >
+                  Install
+                </Button>
+              </Tooltip>
+            </Button.Group>
+          </Stack>
+        </Paper>
       </Group>
 
       <Modal
