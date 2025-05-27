24745d0dda2d917dfe4cd77b4d8d66d1856866bb
Dominik Förderreuther <dominik@eleon.de>
2025-05-27 18:39:35 +0200

    publisher replication agent (flush)

--- src/main/services/ReplicationSettings.ts
+++ src/main/services/ReplicationSettings.ts
@@ -17,13 +17,136 @@ export class ReplicationSettings {
         }
         return ReplicationSettings.instance;
     }
-
     async setReplication(project: Project, instance: 'author' | 'publisher') {
-        // TODO: author replication, publisher dispatcher flush
+        if (instance === 'author') {
+            await this.setReplicationAuthor(project, instance);
+        } else {
+            await this.setReplicationPublisher(project, instance);
+        }
+    }
+
+    private async setReplicationPublisher(project: Project, instance: 'author' | 'publisher') {
+        console.log('setReplicationPublisher', project, instance);
+
+        let data = '/sling:resourceType=cq/replication/components/agent&' + 
+        './jcr:lastModified=&' + 
+        './jcr:lastModifiedBy=&' + 
+        '_charset_=utf-8&' + 
+        ':status=browser&' + 
+        './jcr:title=Dispatcher Flush (aem-start)&' + 
+        './jcr:description=Example agent that is triggered on modification and sends flush requests to the dispatcher.&' + 
+        './enabled=true&./enabled@Delete=&' + 
+        './serializationType=flush&' + 
+        './retryDelay=60000&' + 
+        './userId=&' + 
+        './logLevel=error&' + 
+        './reverseReplication@Delete=&' + 
+        './transportUri=http://wknd.local/dispatcher/invalidate.cache&' + 
+        './transportUser=&' + 
+        './transportPassword=&' + 
+        './transportNTLMDomain=&' + 
+        './transportNTLMHost=&' + 
+        './ssl=default&' + 
+        './protocolHTTPExpired@Delete=&' + 
+        './proxyHost=&' + 
+        './proxyPort=&' + 
+        './proxyUser=&' + 
+        './proxyPassword=&' + 
+        './proxyNTLMDomain=&' + 
+        './proxyNTLMHost=&' + 
+        './protocolInterface=&' + 
+        './protocolHTTPMethod=GET&' + 
+        './protocolHTTPHeaders=CQ-Action:{action}&' + 
+        './protocolHTTPHeaders=CQ-Handle:{path}&' + 
+        './protocolHTTPHeaders=CQ-Path: {path}&' + 
+        './protocolHTTPHeaders@Delete=&' + 
+        './protocolHTTPConnectionClose@Delete=true&' + 
+        './protocolConnectTimeout=&' + 
+        './protocolSocketTimeout=&' + 
+        './protocolVersion=&' + 
+        './triggerSpecific=true&' + 
+        './triggerSpecific@Delete=&' + 
+        './triggerModified=true&' + 
+        './triggerModified@Delete=&' + 
+        './triggerDistribute=true&' + 
+        './triggerDistribute@Delete=&' + 
+        './triggerOnOffTime=true&' + 
+        './triggerOnOffTime@Delete=&' + 
+        './triggerReceive=true&' + 
+        './triggerReceive@Delete=&' + 
+        './noStatusUpdate@Delete=&' + 
+        './noVersioning=true&' + 
+        './noVersioning@Delete=&' + 
+        './queueBatchMode@Delete=&' + 
+        './queueBatchWaitTime=&' + 
+        './queueBatchMaxSize=';
+        
+        let dataRaw = encodeURI(data);
+        let command = "curl -u admin:admin 'http://localhost:4503/etc/replication/agents.publish/flush/jcr:content' " + 
+                    "-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' " + 
+                    "--data-raw '." + dataRaw + "'"
+
+        try {
+            const { stdout, stderr } = await execAsync(command);
+            console.log('Replication agent configured successfully:', stdout);
+            return { success: true, output: stdout };
+        } catch (error) {
+            console.error('Error configuring replication agent:', error);
+            return { success: false, error: error };
+        }
+    }
+
+    private async setReplicationAuthor(project: Project, instance: 'author' | 'publisher') {
         // TODO: dynamic ports
+        let data = '/sling:resourceType=cq/replication/components/agent&' + 
+        './jcr:lastModified=&' + 
+        './jcr:lastModifiedBy=&' + 
+        '_charset_=utf-8&' + 
+        ':status=browser&' + 
+        './jcr:title=Default Agent (aem-start)&' + 
+        './jcr:description=Agent that replicates to the default publish instance.&' + 
+        './enabled=true&' + 
+        './enabled@Delete=&' + 
+        './serializationType=durbo&' + 
+        './retryDelay=60000&' + 
+        './userId=&' + 
+        './logLevel=info&' + 
+        './reverseReplication@Delete=&' + 
+        './transportUri=http://localhost:4503/bin/receive?sling:authRequestLogin=1&' + 
+        './transportUser=admin&' + 
+        './transportPassword=admin&' + 
+        './transportNTLMDomain=&' + 
+        './transportNTLMHost=&' + 
+        './ssl=&' + 
+        './protocolHTTPExpired@Delete=&' + 
+        './proxyHost=&' + 
+        './proxyPort=&' + 
+        './proxyUser=&' + 
+        './proxyPassword=&' + 
+        './proxyNTLMDomain=&' + 
+        './proxyNTLMHost=&' + 
+        './protocolInterface=&' + 
+        './protocolHTTPMethod=&' + 
+        './protocolHTTPHeaders@Delete=&' + 
+        './protocolHTTPConnectionClose@Delete=true&' + 
+        './protocolConnectTimeout=&' + 
+        './protocolSocketTimeout=&' + 
+        './protocolVersion=&' + 
+        './triggerSpecific@Delete=&' + 
+        './triggerModified@Delete=&' + 
+        './triggerDistribute@Delete=&' + 
+        './triggerOnOffTime@Delete=&' + 
+        './triggerReceive@Delete=&' + 
+        './noStatusUpdate@Delete=&' + 
+        './noVersioning@Delete=&' + 
+        './queueBatchMode@Delete=&' + 
+        './queueBatchWaitTime=&' + 
+        './queueBatchMaxSize=';
+        
+        let dataRaw = encodeURI(data);
         let command = "curl -u admin:admin 'http://localhost:4502/etc/replication/agents.author/publish/jcr:content' " + 
                     "-H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' " + 
-                    "--data-raw '.%2Fsling%3AresourceType=cq%2Freplication%2Fcomponents%2Fagent&.%2Fjcr%3AlastModified=&.%2Fjcr%3AlastModifiedBy=&_charset_=utf-8&%3Astatus=browser&.%2Fjcr%3Atitle=Default%20Agent3&.%2Fjcr%3Adescription=Agent%20that%20replicates%20to%20the%20default%20publish%20instance.&.%2Fenabled=true&.%2Fenabled%40Delete=&.%2FserializationType=durbo&.%2FretryDelay=60000&.%2FuserId=&.%2FlogLevel=info&.%2FreverseReplication%40Delete=&.%2FtransportUri=http%3A%2F%2Flocalhost%3A4503%2Fbin%2Freceive%3Fsling%3AauthRequestLogin%3D1&.%2FtransportUser=admin&.%2FtransportPassword=admin&.%2FtransportNTLMDomain=&.%2FtransportNTLMHost=&.%2Fssl=&.%2FprotocolHTTPExpired%40Delete=&.%2FproxyHost=&.%2FproxyPort=&.%2FproxyUser=&.%2FproxyPassword=&.%2FproxyNTLMDomain=&.%2FproxyNTLMHost=&.%2FprotocolInterface=&.%2FprotocolHTTPMethod=&.%2FprotocolHTTPHeaders%40Delete=&.%2FprotocolHTTPConnectionClose%40Delete=true&.%2FprotocolConnectTimeout=&.%2FprotocolSocketTimeout=&.%2FprotocolVersion=&.%2FtriggerSpecific%40Delete=&.%2FtriggerModified%40Delete=&.%2FtriggerDistribute%40Delete=&.%2FtriggerOnOffTime%40Delete=&.%2FtriggerReceive%40Delete=&.%2FnoStatusUpdate%40Delete=&.%2FnoVersioning%40Delete=&.%2FqueueBatchMode%40Delete=&.%2FqueueBatchWaitTime=&.%2FqueueBatchMaxSize='"
+                    "--data-raw '." + dataRaw + "'"
 
         try {
             const { stdout, stderr } = await execAsync(command);
